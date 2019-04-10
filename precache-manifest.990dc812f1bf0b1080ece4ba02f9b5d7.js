6c791d9cadd638f8a92a0ace3a15e5e95686f251
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-04-10 19:49:13 +0300

    Updates

---
+++ precache-manifest.990dc812f1bf0b1080ece4ba02f9b5d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "586f7a3b1f5b6259e5f0",
+    "url": "/internet-store/static/js/runtime~main.abd69102.js"
+  },
+  {
+    "revision": "189d3d803f1bdaa7d315",
+    "url": "/internet-store/static/js/main.2dfc837d.chunk.js"
+  },
+  {
+    "revision": "682f71cc5e0ad217d3e6",
+    "url": "/internet-store/static/js/2.42007a46.chunk.js"
+  },
+  {
+    "revision": "189d3d803f1bdaa7d315",
+    "url": "/internet-store/static/css/main.29ed59aa.chunk.css"
+  },
+  {
+    "revision": "28692c938bfb15c1ab81a2e74cf61f90",
+    "url": "/internet-store/index.html"
+  }
+];
